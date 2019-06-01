2c626702fc6ee5646761641ab92199a6452eca7e
Troll Mann <tmann@mi-l5.21-school.ru>
2019-06-01 15:30:01 +0300

    norm 100%

--- src/run_of_ants.c
+++ src/run_of_ants.c
@@ -6,7 +6,7 @@
 /*   By: tmann <tmann@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 13:00:41 by dspider-          #+#    #+#             */
-/*   Updated: 2019/05/31 22:06:55 by tmann            ###   ########.fr       */
+/*   Updated: 2019/06/01 15:14:41 by tmann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,11 +40,21 @@ void		vitya_create_name(t_lst *lst)
 	}
 }
 
-void		ants_go_output(t_lem *po, t_lst *hl, int i, t_lst *end)
+t_lst		*vitya_create(t_lst *lst)
+{
+	while (lst->go != NULL)
+	{
+		vitya_create_name(lst);
+		lst = lst->go;
+	}
+	return (lst);
+}
+
+void		ants_go_output(t_lem *po, t_lst *hl, int k, t_lst *end)
 {
 	t_lst	*lst;
 	int		left;
-	int		k;
+	int		i;
 
 	while (end->end != 1)
 	{
@@ -52,18 +62,13 @@ void		ants_go_output(t_lem *po, t_lst *hl, int i, t_lst *end)
 		end = end->next;
 	}
 	left = po->num_ants;
-	k = 1;
 	while (po->num_ants != end->ant)
 	{
 		i = 0;
 		while (i < hl->sizeconn)
 		{
 			lst = hl->conn[i];
-			while (lst->go != NULL)
-			{
-				vitya_create_name(lst);
-				lst = lst->go;
-			}
+			lst = vitya_create(lst);
 			lst = hl->conn[i];
 			if (lst->go != NULL && left > 0)
 				vitya_create_name2(lst, &left, &k);
